cee44b31226794f90a6abada70c19ba8eae459dc
Eman <eman1371994@gmail.com>
2020-02-19 12:07:53 +0200

    added js file

---
+++ amir/js/javascript.js
@@ -0,0 +1,120 @@
+function getData(lang) {
+    console.log("here test" + lang)
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            var myObj = JSON.parse(xhttp.response);
+            if (lang == 'EN') {
+                var obj = myObj.EN;
+                localStorage.setItem("langObj", JSON.stringify(obj));
+                window.document.location = './chooseEntries.html'
+                document.getElementById("demo").innerHTML = obj.code;
+
+                console.log(obj.UPS);
+            } else if (lang == 'AR') {
+                var obj = myObj.AR;
+                localStorage.setItem("langObj", JSON.stringify(obj));
+                window.document.location = './chooseEntries.html'
+                document.getElementById("demo").innerHTML = obj.code;
+
+                console.log(obj.UPS);
+            } else if (lang == 'HE') {
+                var obj = myObj.HE;
+                localStorage.setItem("langObj", JSON.stringify(obj));
+                window.document.location = './chooseEntries.html'
+                document.getElementById("demo").innerHTML = obj.code;
+
+                console.log(obj.UPS);
+            } else if (lang == 'RU') {
+                var obj = myObj.RU;
+                localStorage.setItem("langObj", JSON.stringify(obj));
+                window.document.location = './chooseEntries.html'
+                document.getElementById("demo").innerHTML = obj.code;
+
+                console.log(obj.UPS);
+            }
+
+
+        }
+    };
+    xhttp.open("GET", "lang.json", true);
+    xhttp.send();
+
+}
+
+function ChangeLanguageForChooseEntries() {
+    var lang = localStorage.getItem("langObj");
+    console.log("e " + JSON.parse(lang).code)
+    var language = JSON.parse(lang);
+    var page3 = language.page3;
+    //console.log(page);
+    document.getElementById("opt1").innerHTML = page3.opt1;
+    document.getElementById("opt2").innerHTML = page3.opt2;
+    var page33 = language.page33;
+    document.getElementById("checkboxText").innerHTML = page33.checkboxText;
+}
+
+function changeLanguageForIDScanner() {
+    var lang = localStorage.getItem("langObj");
+    console.log("e " + JSON.parse(lang).code)
+    var language = JSON.parse(lang);
+    var page = language.page41;
+    // console.log(page);
+    document.getElementById("title").innerHTML = page.titlephoto;
+    document.getElementById("subtitle3").innerHTML = page.subtitle3;
+    document.getElementById("startscan").innerHTML = page.startscan;
+
+}
+
+function changeLanguageForCustomerNotFound() {
+    var lang = localStorage.getItem("langObj");
+    console.log("e " + JSON.parse(lang).code)
+    var language = JSON.parse(lang);
+    var page = language.page312;
+    // console.log(page);
+    document.getElementById("m211").innerHTML = page.m211;
+    document.getElementById("m2112").innerHTML = page.m2112;
+    document.getElementById("m2113").innerHTML = page.m2113;
+
+}
+document.addEventListener('DOMContentLoaded', (ev) => {
+    let form = document.getElementById('myform');
+    //get the captured media file
+    let input = document.getElementById('scan');
+
+    input.addEventListener('change', (ev) => {
+        console.dir(input.files[0]);
+        if (input.files[0].type.indexOf("image/") > -1) {
+            let img = document.getElementById('img');
+            img.src = window.URL.createObjectURL(input.files[0]);
+        }
+
+
+
+
+    })
+
+})
+
+
+function clickCounter() {
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+    }
+}
+
+function hideme() {
+    if (localStorage.clickcount >= 3) {
+        document.getElementsByClassName('page-button')[1].style.visibility = "visible"
+
+        document.getElementsByClassName('page-button')[0].onclick = "";
+
+        localStorage.clear();
+    } else {
+        document.getElementsByClassName('page-button')[1].style.visibility = "hidden"
+    }
+}
