28987ff5a004a78ce262a78eeeee21bb579412c1
Eman <eman1371994@gmail.com>
2020-02-23 14:35:48 +0200

    update all pages with language

--- amir/js/javascript.js
+++ amir/js/javascript.js
@@ -166,3 +166,159 @@ function blurFunction() {
         }
     }
 }
+
+function changeLanguageForOrderDetails() {
+    var lang = localStorage.getItem("langObj");
+    console.log("e " + JSON.parse(lang).code)
+    var language = JSON.parse(lang);
+    var page = language.page41;
+    console.log(page);
+
+    document.getElementById("title1").innerHTML = page.title1;
+    document.getElementById("next1").innerHTML = page.next1;
+    document.getElementById("again1").innerHTML = page.again1;
+
+
+
+}
+
+function changeLanguageForNewInfo() {
+    var lang = localStorage.getItem("langObj");
+    console.log("e " + JSON.parse(lang).code)
+    var language = JSON.parse(lang);
+    var page = language.page9;
+    console.log(page);
+    document.getElementById("title").innerHTML = page.title;
+
+    document.getElementById("opt1").innerHTML = page.opt1;
+    document.getElementById("opt2").innerHTML = page.opt2;
+    document.getElementById("opt4").innerHTML = page.opt4;
+    document.getElementById("opt5").innerHTML = page.opt5;
+    document.getElementById("ok").innerHTML = page.ok;
+    document.getElementById("opt3").innerHTML = page.opt3;
+
+
+
+}
+
+function changeLanguageForReceiptCode() {
+    var lang = localStorage.getItem("langObj");
+    console.log("e " + JSON.parse(lang).code)
+    var language = JSON.parse(lang);
+    var page = language.page10;
+    console.log(page);
+    document.getElementById("title").innerHTML = page.title;
+
+    document.getElementById("subtitle").innerHTML = page.subtitle;
+
+
+
+
+}
+
+function changeLanguageForCitizenShip() {
+    var lang = localStorage.getItem("langObj");
+    console.log("e " + JSON.parse(lang).code)
+    var language = JSON.parse(lang);
+    var page = language.page41;
+    console.log(page);
+    document.getElementById("titlecitizen").innerHTML = page.titlecitizen;
+
+    document.getElementById("choose").innerHTML = page.choose;
+    document.getElementById("citizen").innerHTML = page.citizen;
+    document.getElementById("nocitizen").innerHTML = page.nocitizen;
+
+
+
+
+}
+
+function changeLanguageForNumberOfPeopleInRoom1() {
+    var lang = localStorage.getItem("langObj");
+    console.log("e " + JSON.parse(lang).code)
+    var language = JSON.parse(lang);
+    var page = language.page41;
+    console.log(page);
+    document.getElementById("title").innerHTML = page.title;
+
+    document.getElementById("choose").innerHTML = page.choose;
+    document.getElementById("opt1").innerHTML = page.opt1;
+    document.getElementById("opt2").innerHTML = page.opt2;
+    document.getElementById("opt3").innerHTML = page.opt3;
+
+
+
+
+}
+
+function changeLanguageForNumberOfPeopleInRoom2() {
+    var lang = localStorage.getItem("langObj");
+    console.log("e " + JSON.parse(lang).code)
+    var language = JSON.parse(lang);
+    var page = language.page14;
+    console.log(page);
+    document.getElementById("title").innerHTML = page.title;
+
+    document.getElementById("subtitle").innerHTML = page.subtitle;
+    document.getElementById("opt1").innerHTML = page.opt1;
+    document.getElementById("opt2").innerHTML = page.opt2;
+    document.getElementById("ok").innerHTML = page.ok;
+
+
+
+
+}
+
+function changeLanguageForTypingFullName() {
+    var lang = localStorage.getItem("langObj");
+    console.log("e " + JSON.parse(lang).code)
+    var language = JSON.parse(lang);
+    var page = language.page15;
+    console.log(page);
+    document.getElementById("info").innerHTML = page.title;
+
+    document.getElementById("inputtitle").innerHTML = page.inputtitle;
+
+    document.getElementById("ok").innerHTML = page.ok;
+
+
+
+
+}
+
+function changeLanguageForChooseDate() {
+    var lang = localStorage.getItem("langObj");
+    console.log("e " + JSON.parse(lang).code)
+    var language = JSON.parse(lang);
+    var page = language.page33;
+    console.log(page);
+    document.getElementById("chosedate").innerHTML = page.title1;
+
+    document.getElementById("dateText1").innerHTML = page.dateText1;
+    document.getElementById("dateText2").innerHTML = page.dateText2;
+
+    document.getElementById("ok").innerHTML = page.ok;
+
+
+
+
+}
+
+function changeLanguageForChooseRoom() {
+    var lang = localStorage.getItem("langObj");
+    console.log("e " + JSON.parse(lang).code)
+    var language = JSON.parse(lang);
+    var page = language.page9;
+    console.log(page);
+    document.getElementById("title").innerHTML = page.title2;
+
+    document.getElementById("opt4").innerHTML = page.opt4;
+    document.getElementById("opt5").innerHTML = page.opt5;
+    document.getElementById("opt3").innerHTML = page.opt3;
+
+    document.getElementById("ok").innerHTML = page.ok;
+
+
+
+
+}
