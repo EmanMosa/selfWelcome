de4b822bf5bff8f828a677bd02a0df0867c2767d
Eman <eman1371994@gmail.com>
2020-02-24 11:12:52 +0200

    i change in page6+7 and java script file
    i change in page6+7 and java script file

--- amir/js/javascript.js
+++ amir/js/javascript.js
@@ -109,6 +109,10 @@ function hideme() {
 
 
 
+
+
+
+
 function changeLanguageForIdentifyingInformation() {
     var lang = localStorage.getItem("langObj");
     console.log("e " + JSON.parse(lang).code)
@@ -125,220 +129,250 @@ function changeLanguageForIdentifyingInformation() {
 
 }
 
+function putplaceholder() {
+    var lang = localStorage.getItem("langObj");
+    console.log("e " + JSON.parse(lang).code)
+    var language = JSON.parse(lang);
+    var page = language.page31;
+    console.log(page);
 
-
-function OnClickForButton() {
     var elements = document.getElementsByClassName('accordion-item accordion-active');
     var id = elements[0].dataset.actabId;
     if (id == "0") {
-        var value = document.getElementById('invitationbyNumber').value;
-    } else {
-        if (id == "1") {
-            var value = document.getElementById('invitationByName').value;
-        }
-    }
-}
-
-function focusFunction() {
-    var elements = document.getElementsByClassName('accordion-item accordion-active');
-    var id = elements[0].dataset.actabId;
-    if (id == "0") {
-        document.getElementById("invitationbyNumber").placeholder = '';
+        document.getElementById("invitationbyNumber").placeholder = page.opt8;;
 
     } else {
         if (id == "1") {
-            // Focus = Changes the background color of input to yellow
-            document.getElementById("invitationByName").placeholder = '';
+
+            document.getElementById("invitationByName").placeholder = page.opt9;
         }
     }
-}
 
-function blurFunction() {
-    var elements = document.getElementsByClassName('accordion-item accordion-active');
-    var id = elements[0].dataset.actabId;
-    if (id == "0") {
-        document.getElementById("invitationbyNumber").placeholder = 'הקלד מספר הזמנה';
-
-    } else {
-        if (id == "1") {
-            // No focus = Changes the background color of input to red
-            document.getElementById("invitationByName").placeholder = " שם משפחה ופרטי כפי שהוקלד בהזמנה "
+    function OnClickForButton() {
+        var elements = document.getElementsByClassName('accordion-item accordion-active');
+        var id = elements[0].dataset.actabId;
+        if (id == "0") {
+            var value = document.getElementById('invitationbyNumber').value;
+        } else {
+            if (id == "1") {
+                var value = document.getElementById('invitationByName').value;
+            }
         }
     }
-}
 
-function changeLanguageForOrderDetails() {
-    var lang = localStorage.getItem("langObj");
-    console.log("e " + JSON.parse(lang).code)
-    var language = JSON.parse(lang);
-    var page = language.page41;
-    console.log(page);
+    function focusFunction() {
+        var elements = document.getElementsByClassName('accordion-item accordion-active');
+        var id = elements[0].dataset.actabId;
+        if (id == "0") {
+            document.getElementById("invitationbyNumber").placeholder = '';
 
-    document.getElementById("title1").innerHTML = page.title1;
-    document.getElementById("next1").innerHTML = page.next1;
-    document.getElementById("again1").innerHTML = page.again1;
+        } else {
+            if (id == "1") {
+                // Focus = Changes the background color of input to yellow
+                document.getElementById("invitationByName").placeholder = '';
+            }
+        }
+    }
 
+    function blurFunction() {
+        var lang = localStorage.getItem("langObj");
+        console.log("e " + JSON.parse(lang).code)
+        var language = JSON.parse(lang);
+        var page = language.page31;
+        console.log(page);
 
+        var elements = document.getElementsByClassName('accordion-item accordion-active');
+        var id = elements[0].dataset.actabId;
+        if (id == "0") {
+            // document.getElementById("invitationbyNumber").innerHTML = page.opt8;
+            document.getElementById("invitationbyNumber").placeholder = page.opt8;;
 
-}
+        } else {
+            if (id == "1") {
+                //document.getElementById("invitationByName").innerHTML = page.opt9;
 
-function changeLanguageForNewInfo() {
-    var lang = localStorage.getItem("langObj");
-    console.log("e " + JSON.parse(lang).code)
-    var language = JSON.parse(lang);
-    var page = language.page9;
-    console.log(page);
-    document.getElementById("title").innerHTML = page.title;
+                // No focus = Changes the background color of input to red
+                document.getElementById("invitationByName").placeholder = page.opt9;
+            }
+        }
 
-    document.getElementById("opt1").innerHTML = page.opt1;
-    document.getElementById("opt2").innerHTML = page.opt2;
-    document.getElementById("opt4").innerHTML = page.opt4;
-    document.getElementById("opt5").innerHTML = page.opt5;
-    document.getElementById("ok").innerHTML = page.ok;
-    document.getElementById("opt3").innerHTML = page.opt3;
 
 
 
-}
 
-function changeLanguageForReceiptCode() {
-    var lang = localStorage.getItem("langObj");
-    console.log("e " + JSON.parse(lang).code)
-    var language = JSON.parse(lang);
-    var page = language.page10;
-    console.log(page);
-    document.getElementById("title").innerHTML = page.title;
 
-    document.getElementById("subtitle").innerHTML = page.subtitle;
+        function changeLanguageForOrderDetails() {
+            var lang = localStorage.getItem("langObj");
+            console.log("e " + JSON.parse(lang).code)
+            var language = JSON.parse(lang);
+            var page = language.page41;
+            console.log(page);
 
+            document.getElementById("title1").innerHTML = page.title1;
+            document.getElementById("next1").innerHTML = page.next1;
+            document.getElementById("again1").innerHTML = page.again1;
 
 
 
-}
+        }
 
-function changeLanguageForCitizenShip() {
-    var lang = localStorage.getItem("langObj");
-    console.log("e " + JSON.parse(lang).code)
-    var language = JSON.parse(lang);
-    var page = language.page41;
-    console.log(page);
-    document.getElementById("titlecitizen").innerHTML = page.titlecitizen;
+        function changeLanguageForNewInfo() {
+            var lang = localStorage.getItem("langObj");
+            console.log("e " + JSON.parse(lang).code)
+            var language = JSON.parse(lang);
+            var page = language.page9;
+            console.log(page);
+            document.getElementById("title").innerHTML = page.title;
 
-    document.getElementById("choose").innerHTML = page.choose;
-    document.getElementById("citizen").innerHTML = page.citizen;
-    document.getElementById("nocitizen").innerHTML = page.nocitizen;
+            document.getElementById("opt1").innerHTML = page.opt1;
+            document.getElementById("opt2").innerHTML = page.opt2;
+            document.getElementById("opt4").innerHTML = page.opt4;
+            document.getElementById("opt5").innerHTML = page.opt5;
+            document.getElementById("ok").innerHTML = page.ok;
+            document.getElementById("opt3").innerHTML = page.opt3;
 
 
 
+        }
 
-}
+        function changeLanguageForReceiptCode() {
+            var lang = localStorage.getItem("langObj");
+            console.log("e " + JSON.parse(lang).code)
+            var language = JSON.parse(lang);
+            var page = language.page10;
+            console.log(page);
+            document.getElementById("title").innerHTML = page.title;
 
-function changeLanguageForNumberOfPeopleInRoom1() {
-    var lang = localStorage.getItem("langObj");
-    console.log("e " + JSON.parse(lang).code)
-    var language = JSON.parse(lang);
-    var page = language.page13;
-    console.log(page);
-    document.getElementById("title").innerHTML = page.title;
+            document.getElementById("subtitle").innerHTML = page.subtitle;
 
-    document.getElementById("choose").innerHTML = page.choose;
-    document.getElementById("opt1").innerHTML = page.opt1;
-    document.getElementById("opt2").innerHTML = page.opt2;
-    document.getElementById("opt3").innerHTML = page.opt3;
 
 
 
+        }
 
-}
+        function changeLanguageForCitizenShip() {
+            var lang = localStorage.getItem("langObj");
+            console.log("e " + JSON.parse(lang).code)
+            var language = JSON.parse(lang);
+            var page = language.page41;
+            console.log(page);
+            document.getElementById("titlecitizen").innerHTML = page.titlecitizen;
 
-function changeLanguageForNumberOfPeopleInRoom2() {
-    var lang = localStorage.getItem("langObj");
-    console.log("e " + JSON.parse(lang).code)
-    var language = JSON.parse(lang);
-    var page = language.page14;
-    console.log(page);
-    document.getElementById("title").innerHTML = page.title;
+            document.getElementById("choose").innerHTML = page.choose;
+            document.getElementById("citizen").innerHTML = page.citizen;
+            document.getElementById("nocitizen").innerHTML = page.nocitizen;
 
-    document.getElementById("subtitle").innerHTML = page.subtitle;
-    document.getElementById("opt1").innerHTML = page.opt1;
-    document.getElementById("opt2").innerHTML = page.opt2;
-    document.getElementById("ok").innerHTML = page.ok;
 
 
 
+        }
 
-}
+        function changeLanguageForNumberOfPeopleInRoom1() {
+            var lang = localStorage.getItem("langObj");
+            console.log("e " + JSON.parse(lang).code)
+            var language = JSON.parse(lang);
+            var page = language.page13;
+            console.log(page);
+            document.getElementById("title").innerHTML = page.title;
 
-function changeLanguageForTypingFullName() {
-    var lang = localStorage.getItem("langObj");
-    console.log("e " + JSON.parse(lang).code)
-    var language = JSON.parse(lang);
-    var page = language.page15;
-    console.log(page);
-    document.getElementById("info").innerHTML = page.title;
+            document.getElementById("choose").innerHTML = page.choose;
+            document.getElementById("opt1").innerHTML = page.opt1;
+            document.getElementById("opt2").innerHTML = page.opt2;
+            document.getElementById("opt3").innerHTML = page.opt3;
 
-    document.getElementById("inputtitle").innerHTML = page.inputtitle;
 
-    document.getElementById("ok").innerHTML = page.ok;
 
 
+        }
 
+        function changeLanguageForNumberOfPeopleInRoom2() {
+            var lang = localStorage.getItem("langObj");
+            console.log("e " + JSON.parse(lang).code)
+            var language = JSON.parse(lang);
+            var page = language.page14;
+            console.log(page);
+            document.getElementById("title").innerHTML = page.title;
 
-}
+            document.getElementById("subtitle").innerHTML = page.subtitle;
+            document.getElementById("opt1").innerHTML = page.opt1;
+            document.getElementById("opt2").innerHTML = page.opt2;
+            document.getElementById("ok").innerHTML = page.ok;
 
 
-function OnClickForButton2() {
 
-    var value = document.getElementById('TypingName').value;
 
-}
+        }
 
-function focusFunctionforTypingName() {
+        function changeLanguageForTypingFullName() {
+            var lang = localStorage.getItem("langObj");
+            console.log("e " + JSON.parse(lang).code)
+            var language = JSON.parse(lang);
+            var page = language.page15;
+            console.log(page);
+            document.getElementById("info").innerHTML = page.title;
 
-    document.getElementById("TypingName").placeholder = '';
+            document.getElementById("inputtitle").innerHTML = page.inputtitle;
 
-}
+            document.getElementById("ok").innerHTML = page.ok;
 
-function blurFunctionforTypingName() {
 
-    document.getElementById("TypingName").placeholder = "הקלדת שם משפחה ושם פרטי";
 
 
-}
+        }
 
-function changeLanguageForChooseDate() {
-    var lang = localStorage.getItem("langObj");
-    console.log("e " + JSON.parse(lang).code)
-    var language = JSON.parse(lang);
-    var page = language.page33;
-    console.log(page);
-    document.getElementById("chosedate").innerHTML = page.title1;
 
-    document.getElementById("dateText1").innerHTML = page.dateText1;
-    document.getElementById("dateText2").innerHTML = page.dateText2;
+        function OnClickForButton2() {
 
-    document.getElementById("ok").innerHTML = page.ok;
+            var value = document.getElementById('TypingName').value;
 
+        }
 
+        function focusFunctionforTypingName() {
 
+            document.getElementById("TypingName").placeholder = '';
 
-}
+        }
 
-function changeLanguageForChooseRoom() {
-    var lang = localStorage.getItem("langObj");
-    console.log("e " + JSON.parse(lang).code)
-    var language = JSON.parse(lang);
-    var page = language.page9;
-    console.log(page);
-    document.getElementById("title").innerHTML = page.title2;
+        function blurFunctionforTypingName() {
 
-    document.getElementById("opt4").innerHTML = page.opt4;
-    document.getElementById("opt5").innerHTML = page.opt5;
-    document.getElementById("opt3").innerHTML = page.opt3;
+            document.getElementById("TypingName").placeholder = "הקלדת שם משפחה ושם פרטי";
 
-    document.getElementById("ok").innerHTML = page.ok;
 
+        }
 
+        function changeLanguageForChooseDate() {
+            var lang = localStorage.getItem("langObj");
+            console.log("e " + JSON.parse(lang).code)
+            var language = JSON.parse(lang);
+            var page = language.page33;
+            console.log(page);
+            document.getElementById("chosedate").innerHTML = page.title1;
 
+            document.getElementById("dateText1").innerHTML = page.dateText1;
+            document.getElementById("dateText2").innerHTML = page.dateText2;
 
-}
+            document.getElementById("ok").innerHTML = page.ok;
+
+
+
+
+        }
+
+        function changeLanguageForChooseRoom() {
+            var lang = localStorage.getItem("langObj");
+            console.log("e " + JSON.parse(lang).code)
+            var language = JSON.parse(lang);
+            var page = language.page9;
+            console.log(page);
+            document.getElementById("title").innerHTML = page.title2;
+
+            document.getElementById("opt4").innerHTML = page.opt4;
+            document.getElementById("opt5").innerHTML = page.opt5;
+            document.getElementById("opt3").innerHTML = page.opt3;
+
+            document.getElementById("ok").innerHTML = page.ok;
+
+
+
+
+        }
